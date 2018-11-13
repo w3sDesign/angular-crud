84e9f094dc79c73a795ef57a5dbb8d2018aa8096
gfranke <gfranke@w3sdesign.com>
2018-11-13 18:57:02 +0100

    Customer

---
+++ src/app/customer/customer.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CustomerEditComponent } from './customer-edit/customer-edit.component';
+
+@NgModule({
+  declarations: [CustomerEditComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CustomerModule { }
